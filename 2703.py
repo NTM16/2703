ae714006431cffdfbdeaf7ac325ac010e17771e4
NTM16 <164037540+NTM16@users.noreply.github.com>
2024-03-30 11:33:12 +0700

    Update 2703.py

--- 2703.py
+++ 2703.py
@@ -531,5 +531,106 @@ if ticker is not None:
 
             final_intrinsic_value = intrinsic_value - debt_per_share + cash_per_share
             st.subheader(f"Final Intrinsic Value per Share: {final_intrinsic_value:.2f}")
+        with guru:
+    st.subheader('Liquidity Ratio')
+    cr_ratio = round((TTM_bsheet['Current Assets'] / TTM_bsheet['Current Liabilities']), 2)
+     # Lấy dữ liệu từ năm trước đến năm hiện tại
+    cr_ratio_history = [bsheet.loc['Current Assets', year] / (bsheet.loc['Current Liabilities', year] or 1) for year in years[::-1]]
+    min_cr_ratio = round(min(cr_ratio_history), 2)
+    max_cr_ratio = round(max(cr_ratio_history), 2)
+
+
+    qr_ratio = round(((TTM_bsheet['Current Assets'] - TTM_bsheet['Inventory'])/TTM_bsheet['Current Liabilities']), 2) if 'Inventory' in TTM_bsheet else TTM_bsheet['Current Assets'] / TTM_bsheet['Current Liabilities']
+    qr_ratio_history = [(bsheet.loc['Current Assets', year] - bsheet.loc['Inventory', year]) / (bsheet.loc['Current Liabilities', year] if 'Inventory' in TTM_bsheet else bsheet['Current Assets', year] / bsheet['Current Liabilities', year]) for year in years[::-1]]
+    # Tính toán giá trị min-max
+    min_qr_ratio = round(min(qr_ratio_history), 2)
+    max_qr_ratio = round(max(qr_ratio_history), 2)
+
+    car_ratio = round((TTM_bsheet['Cash And Cash Equivalents'] / TTM_bsheet['Current Liabilities']), 2)
+    car_ratio_history = [bsheet.loc['Cash And Cash Equivalents', year] / (bsheet.loc['Current Liabilities', year] or 1) for year in years[::-1]]
+    # Tính toán giá trị min-max
+    min_car_ratio = round(min(car_ratio_history), 2)
+    max_car_ratio = round(max(car_ratio_history), 2)
+
+    dso_ratio = round((TTM_bsheet['Accounts Receivable'] / TTM['Total Revenue']) * 365, 2)
+    dso_ratio_history = [bsheet.loc['Accounts Receivable', year] * 365 / (income.loc['Total Revenue', year] or 1) for year in years[::-1]]
+    # Tính toán giá trị min-max
+    min_dso_ratio = round(min(dso_ratio_history), 2)
+    max_dso_ratio = round(max(dso_ratio_history), 2)
+
+    ap_average_values = (TTM_bsheet4['Accounts Payable'] + TTM_bsheet['Accounts Payable'])/2
+    dp_ratio = round((ap_average_values / TTM['Cost Of Revenue']) * 365, 2)
+    dp_ratio_history = [bsheet.loc['Accounts Payable', year] * 365 / (income.loc['Cost Of Revenue', year] or 1) for year in years[::-1]]
+    # Tính toán giá trị min-max
+    min_dp_ratio = round(min(dp_ratio_history), 2)
+    max_dp_ratio = round(max(dp_ratio_history), 2)
+
+    inv_average = (TTM_bsheet4['Inventory'] + TTM_bsheet['Inventory'])/2 if 'Inventory' in TTM_bsheet else 0
+    dio_ratio = round((inv_average / TTM['Cost Of Revenue']) * 365, 2)
+    dio_ratio_history = [bsheet.loc['Inventory', year] * 365 / (income.loc['Cost Of Revenue', year] or 1) for year in years[::-1]]
+    # Tính toán giá trị min-max
+    min_dio_ratio = round(min(dio_ratio_history), 2)
+    max_dio_ratio = round(max(dio_ratio_history), 2)
+
+    div_ratio = round(mck.info['trailingAnnualDividendYield'] * 100, 2)
+    pr_ratio = round(mck.info['payoutRatio'], 2)
+    five_years_ratio = round(mck.info['fiveYearAvgDividendYield'], 2)
+    forward_ratio = round(mck.info['dividendYield'] * 100, 2)
+    cr_values = (cr_ratio - min_cr_ratio) / (max_cr_ratio - min_cr_ratio)
+    qr_values = (qr_ratio - min_qr_ratio) / (max_qr_ratio - min_qr_ratio)
+    car_values = (car_ratio - min_car_ratio) / (max_car_ratio - min_car_ratio)
+    dso_values = (dso_ratio - min_dso_ratio) / (max_dso_ratio - min_dso_ratio)
+    dp_values = (dp_ratio - min_dp_ratio) / (max_dp_ratio - min_dp_ratio)
+    dio_values = (dio_ratio - min_dio_ratio) / (max_dio_ratio - min_dio_ratio)
+    div_values = 0
+    pr_values = 0
+    five_years_values = 0
+    forward_values = 0
+
+    data_liquidity = pd.DataFrame(
+        {
+            "STT": [1, 2, 3, 4, 5, 6],
+            "Index": ['Current Ratio', 'Quick Ratio', 'Cash Ratio', 'Days Inventory', 'Days Sales Outstanding',
+                      'Days Payable'],
+            "Current": [cr_ratio, qr_ratio, car_ratio, dio_ratio, dso_ratio, dp_ratio],
+            "Vs History":[cr_values, qr_values, car_values, dio_values, dso_values, dp_values],
+        }
+    )
+    st.data_editor(
+        data_liquidity,
+        column_config={
+            "Vs History":st.column_config.ProgressColumn(
+                "Vs History",
+            ),
+        },
+        hide_index=True,
+    )
+
+    st.subheader('Dividend & Buy Back')
+    data_dividend = pd.DataFrame(
+        {
+            "STT": [1, 2, 3, 4],
+            "Index": ['Dividend Yield', 'Dividend Payout Ratio', '5-Year Yield-on-Cost', 'Forward Dividend Yield'],
+            "Current": [div_ratio, pr_ratio, five_years_ratio, forward_ratio],
+            "Vs History":[div_values, pr_values, five_years_values, forward_values],
+        }
+    )
+    st.data_editor(
+        data_dividend,
+        column_config={
+            "Vs History":st.column_config.ProgressColumn(
+                "Vs History",
+            ),
+        },
+        hide_index=True,
+    )
+             
+
+
+    
+   
+   
+        
+
 
             
